357b96eb805e42b0db97d5f7f5098bdb6040d0d1
Adnan Yassin Kassab <adnan@Adnans-MacBook-Pro.local>
2022-02-08 18:51:12 -0500

    created a toggle button for background color

--- practice_app/practice_app/ViewController.swift
+++ practice_app/practice_app/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var yellowColor = true
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColor.yellow
         // Do any additional setup after loading the view.
     }
 
-
+//    @IBAction func YellowButton(_ sender: UIButton) {
+//        if yellowColor {
+//            view.backgroundColor = UIColor.red
+//            yellowColor = false
+//        }
+//        else{
+//            view.backgroundColor = UIColor.yellow
+//            yellowColor = true
+//        }
+//
+//    }
+   
+    @IBAction func toggleYellow(_ sender: UISwitch) {
+        if yellowColor {
+            view.backgroundColor = UIColor.red
+            yellowColor = false
+        }
+        else{
+            view.backgroundColor = UIColor.yellow
+            yellowColor = true
+        }
+    }
+    
 }
 
