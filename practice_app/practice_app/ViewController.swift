e643fad388dce459cb0b23337a25cfd93ace54db
Adnan Yassin Kassab <adnan@Adnans-MacBook-Pro.local>
2022-02-08 17:55:55 -0500

    init the repo

---
+++ practice_app/practice_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practice_app
+//
+//  Created by Adnan Yassin Kassab on 2022-02-08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
